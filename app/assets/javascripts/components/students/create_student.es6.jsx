ee18031c2a6dee037e32ce8019ea03e0001c70c1
diamantk <diamantkolshi@gmail.com>
2017-06-17 14:09:37 +0200

    change style of class react and create averageBox element

--- app/assets/javascripts/components/students/create_student.es6.jsx
+++ app/assets/javascripts/components/students/create_student.es6.jsx
@@ -1,11 +1,28 @@
-class CreateStudent extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
+const CreateStudent = React.createClass({
+	getInitialState: function(){
+		return {
+			totalPoint: 0,
+			subjectCount: 0,
+			average: 0.0
+		};
+	},
+
+	handleValueChange: function(e) {
+		this.setState({[e.target.name]: e.target.value});
+	},
+
+	averageRes: function(){
+		result = 0
+		if(this.state.totalPoint != 0 && this.state.subjectCount != 0) {
+			result = this.state.totalPoint / this.state.subjectCount
 		}
-	}
-	
-  render () {
+		else {
+			result = 0
+		}
+		return result
+	},
+
+  render: function() {
     return (
   		<div className="field form-inputs margin-top-50px">
 			  <div className="field-label is-normal">
@@ -25,14 +42,12 @@ class CreateStudent extends React.Component {
 			        <input className="input" style={{width: '200px'}} type="number" placeholder="Age" />
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="number" placeholder="Total Point" />
+			        <input className="input" style={{width: '200px'}} type="number" name="totalPoint" onChange={this.handleValueChange} placeholder="Total Point" />
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="text" placeholder="Subject Number" />
+			        <input className="input" style={{width: '200px'}} type="number" name="subjectCount" onChange={this.handleValueChange} placeholder="Subject Number" />
 			      </p>
-			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="text" placeholder="Average" disabled/>
-			      </p>
+			      <AverageBox average={this.averageRes()}></AverageBox>
 			      <p className="control is-expanded has-icons-left">
 			      	<button className="button is-primary">
 			          Create Student
@@ -43,7 +58,7 @@ class CreateStudent extends React.Component {
 			</div>
     )
   }
-}
+});
 
 
 
