727f8598ce648b19b5a9d3c6139f27ba208aa2e3
diamantk <diamantkolshi@gmail.com>
2017-06-18 03:28:53 +0200

    add create student method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,22 @@ class StudentsController < ApplicationController
   	@students = Student.all
   end
 
+  def create
+  	@student = Student.new(student_params)
+  	if @lift.save
+  		render json: @student
+  	else
+  		render json: @student.errors, status: :unprocessable_entity
+  	end
+  end
+
   def show 
   	@student = Student.first
+  end
+
+  private
+
+  def student_params
+  	params.require(:student).permit(:name, :surname, :age, :total_point, :subject_count, :average)
   end
 end
