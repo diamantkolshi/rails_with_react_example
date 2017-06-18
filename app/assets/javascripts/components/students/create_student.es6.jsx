723517e8b576cab462d93965bd3652411f8e86c2
diamantk <diamantkolshi@gmail.com>
2017-06-18 03:30:17 +0200

    rename state and add handle submit method

--- app/assets/javascripts/components/students/create_student.es6.jsx
+++ app/assets/javascripts/components/students/create_student.es6.jsx
@@ -1,8 +1,11 @@
 const CreateStudent = React.createClass({
 	getInitialState: function(){
 		return {
-			totalPoint: 0,
-			subjectCount: 0,
+			name: '',
+			surname: '',
+			age: '',
+			total_point: 0,
+			subject_count: 0,
 			average: 0.0
 		};
 	},
@@ -12,14 +15,28 @@ const CreateStudent = React.createClass({
 	},
 
 	averageRes: function(){
-		result = 0
-		if(this.state.totalPoint != 0 && this.state.subjectCount != 0) {
-			result = this.state.totalPoint / this.state.subjectCount
+		if(this.state.total_point != 0 && this.state.subject_count != 0) {
+			this.state.average = this.state.total_point / this.state.subject_count
 		}
 		else {
-			result = 0
+			this.state.average = 0
 		}
-		return result
+		return this.state.average
+	},
+
+	handleSubmit: function(e) {
+		e.preventDefault();
+		// $.ajax({ 
+		// 	url: '/students', 
+		// 	type: 'POST',  
+		// 	data: { student: { name: "asdas" }, 
+		// 	success: (response) => { 
+		// 		// return result 
+		// 	});
+	},
+
+	method_1: function() {
+		debugger;
 	},
 
   render: function() {
@@ -30,26 +47,26 @@ const CreateStudent = React.createClass({
 			  <div className="field-body">
 			    <div className="field is-grouped">
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="text" placeholder="Name" />
+			        <input className="input" style={{width: '200px'}} type="text" name="name" onChange={this.handleValueChange} placeholder="Name"  />
 			        <span className="icon is-small is-left">
 			          <i className="fa fa-user"></i>
 			        </span>
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="text" placeholder="Surname" />
+			        <input className="input" style={{width: '200px'}} type="text" name="surname" onChange={this.handleValueChange} placeholder="Surname" />
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="number" placeholder="Age" />
+			        <input className="input" style={{width: '200px'}} type="number" name="age" onChange={this.handleValueChange} placeholder="Age" />
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="number" name="totalPoint" onChange={this.handleValueChange} placeholder="Total Point" />
+			        <input className="input" style={{width: '200px'}} type="number" name="total_point" onChange={this.handleValueChange} placeholder="Total Point" />
 			      </p>
 			      <p className="control is-expanded has-icons-left">
-			        <input className="input" style={{width: '200px'}} type="number" name="subjectCount" onChange={this.handleValueChange} placeholder="Subject Number" />
+			        <input className="input" style={{width: '200px'}} type="number" name="subject_count" onChange={this.handleValueChange} placeholder="Subject Number" />
 			      </p>
 			      <AverageBox average={this.averageRes()}></AverageBox>
 			      <p className="control is-expanded has-icons-left">
-			      	<button className="button is-primary">
+			      	<button className="button is-primary" onClick={this.method_1}>
 			          Create Student
 			        </button>
 			      </p>			      
