c0a5e5c6669cc6ec84cedd53c157f88c864b5c6e
diamantk <diamantkolshi@gmail.com>
2017-06-24 14:34:53 +0200

    call props to handle search

--- app/assets/javascripts/components/students/search_student.es6.jsx
+++ app/assets/javascripts/components/students/search_student.es6.jsx
@@ -1,22 +1,37 @@
 class SearchStudent extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
-		}
-	}
-	
-  render () {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  } 
+
+  handleSearch(e, keyword) {
+    var _this = this
+    e.preventDefault(); 
+    $.ajax({
+      url: "/students/search",
+      type: 'GET',
+      data: { keyword: keyword },
+      success: function(data) {
+        _this.props.handleSearchStudents(data)
+      }
+    });
+  }
+
+  render() {
     return (
-    	<div className="field is-grouped search-item">
-        <p className="control is-expanded">
-          <input className="input" type="text" placeholder="search student"/>
-        </p>
-        <p className="control">
-          <a className="button is-info">
-            Search
-          </a>
-        </p>
-      </div>
+      <form>
+      	<div className="field has-addons search-field">
+          <p className="control">
+            <input className="input" id="search-field" name="kayword" type="text" placeholder="Search student"/>
+          </p>
+          <p className="control">
+            <a className="button is-info" onClick={e => this.handleSearch(e, $('#search-field')[0].value)}>
+              Search
+            </a>
+          </p>
+        </div>
+      </form>
     )
   }
 }
