34216fe4d1cc5f98c90712965be2d42205e7ee5a
diamantk <diamantkolshi@gmail.com>
2017-06-24 14:38:11 +0200

    add studentRow function to return rowsof students

--- app/assets/javascripts/components/students/student_item.es6.jsx
+++ app/assets/javascripts/components/students/student_item.es6.jsx
@@ -59,23 +59,23 @@ const StudentItem = React.createClass({
   },
 
   studentRow: function(){
-      RowBody = <tr>
-              <td>{this.props.student.name}</td>     
-              <td>{this.props.student.surname}</td>
-              <td>{this.props.student.age}</td>
-              <td>{this.props.student.total_point}</td>
-              <td>{this.props.student.subject_count}</td>
-              <td>{this.props.student.average}</td>
-              <td>
-                <button className="button is-info" onClick={this.handleToggl}>
-                  Edit
-                </button>
-                <button className="button is-danger" onClick={this.handleDelete} >
-                  Delete
-                </button>
-              </td>
-            </tr>;
-      return RowBody;
+    RowBody = <tr>
+            <td>{this.props.student.name}</td>     
+            <td>{this.props.student.surname}</td>
+            <td>{this.props.student.age}</td>
+            <td>{this.props.student.total_point}</td>
+            <td>{this.props.student.subject_count}</td>
+            <td>{this.props.student.average}</td>
+            <td>
+              <button className="button is-info" onClick={this.handleToggl}>
+                Edit
+              </button>
+              <button className="button is-danger" onClick={this.handleDelete} >
+                Delete
+              </button>
+            </td>
+          </tr>;
+    return RowBody;
   },
 
   studentForm: function(){
